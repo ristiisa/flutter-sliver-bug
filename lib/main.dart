d8d75318e724d0c025710cb501586c87dbaef56d
Silver Aabjõe <ristiisa@gmail.com>
2024-02-02 14:07:35 +0200

    updated repro code

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,71 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/rendering.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+const MAPBOX_ACCESS_TOKEN = "MAPBOX ACCESS TOKEN HERE";
 
 void main() {
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: SafeArea(child: HomeScreen()),
-      ),
-    );
+    return MaterialApp(home: HomeScreen());
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: CustomScrollView(
-          slivers: getSlivers(),
-        ),
-      ),
-    );
-  }
-
-  // Returns a list of slivers containing a pinned SliverAppBar and Platform View Widgets
-  List<Widget> getSlivers() {
-    final appBar = SliverAppBar(
-      collapsedHeight: 80,
-      forceElevated: true,
-      backgroundColor: Colors.black,
-      pinned: true,
-      title: Text("Sliver App Bar"),
-    );
-
-    final bodySlivers = SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (c, i) {
-          if (i % 2 == 0) {
-            return Container(height: 120, child: PlatformViewWidget());
-          } else {
-            return Container(
-              height: 120,
-              color: Colors.grey,
-              child: Center(child: Text("Flutter Widget")),
-            );
-          }
-        },
-        childCount: 16,
-      ),
-    );
-
-    return [appBar, SliverPersistentHeaderWidget(), bodySlivers];
-  }
-}
-
-class SliverPersistentHeaderWidget extends StatefulWidget {
-  const SliverPersistentHeaderWidget({Key? key}) : super(key: key);
-
-  @override
-  _SliverPersistentHeaderWidgetState createState() =>
-      _SliverPersistentHeaderWidgetState();
-}
-
-class _SliverPersistentHeaderWidgetState
-    extends State<SliverPersistentHeaderWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return SliverPersistentHeader(
-      pinned: true,
-      delegate: _SliverAppBarDelegate(
-        Container(
-          width: double.infinity,
-          height: 80,
-          color: Colors.amber,
-          child: Center(child: Text("Sliver Persistent Header")),
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            pinned: true,
+            title: Text('SliverAppBar'),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return index.isEven
+                  ? Container(height: 120, child: PlatformViewWidget())
+                  : Container(
+                      height: 120,
+                      child: Text('Text from Flutter'),
+                    );
+              },
+              childCount: 1024,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  final Widget child;
-
-  _SliverAppBarDelegate(this.child);
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return child;
-  }
-
-  @override
-  double get maxExtent => 80;
-
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
-  }
-}
-
-class PlatformViewWidget extends StatefulWidget {
-  const PlatformViewWidget({Key? key}) : super(key: key);
-
-  @override
-  _PlatformViewWidgetState createState() => _PlatformViewWidgetState();
-}
-
-class _PlatformViewWidgetState extends State<PlatformViewWidget> {
+class PlatformViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Map<String, dynamic> creationParams = <String, dynamic>{};
     if (Platform.isIOS) {
-      return UiKitView(
-        viewType: "testView",
-        layoutDirection: TextDirection.ltr,
-        creationParams: creationParams,
-        creationParamsCodec: const StandardMessageCodec(),
-      );
+      return Row(children: [
+        Expanded(child: UiKitView(
+          viewType: "testView",
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+        )),
+        Expanded(child: MapWidget(
+          resourceOptions: ResourceOptions(accessToken: MAPBOX_ACCESS_TOKEN),
+        ))
+      ]);
     }
+
     return Container();
   }
 }
